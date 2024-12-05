e860ec60deab5eb2ed490c26eaa56b57a5536297
Rege-97 <cogus1027@gmail.com>
2024-12-05 16:53:32 +0900

    study : Cafe 2024.12.05

---
+++ practice/src/Ex24.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Ex24 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] strArr= {"가위","바위","보"};
+		System.out.println(Arrays.toString(strArr));
+		
+		for(int i=0;i<10;i++) {
+			int tmp =(int)(Math.random()*3);
+			
+			System.out.println(strArr[tmp]);
+		}
+	}
+
+}

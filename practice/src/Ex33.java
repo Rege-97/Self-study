c4a284149f04787ba466312dbdecbe4a590ebd9f
Rege-97 <cogus1027@gmail.com>
2024-12-21 17:06:52 +0900

    Study : 2024.12.21 -1

--- practice/src/Ex33.java
+++ practice/src/Ex33.java
@@ -2,24 +2,29 @@ import java.io.*;
 
 public class Ex33 {
 
-	public static void main(String[] args)throws IOException {
-		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		System.out.print("숫자 입력 : ");
-		int user=Integer.parseInt(br.readLine());
-		int sum=0;
-		for(int i=1;i<=user;i++) {
-			if(i==1) {
+		int user = Integer.parseInt(br.readLine());
+	
+		int sum = 0;
+		
+		for (int i = 1; i <= user; i++) {
+			if (i == 1) {
 				System.out.print("1");
-				sum+=1;
-			}else if(i%2==0) {
-				System.out.print(" - "+i);
-				sum-=i;
-			}else {
-				System.out.print(" + "+i);
-				sum+=i;
+				sum += 1;
+			} else if (i % 2 == 0) {
+				System.out.print(" - " + i);
+				sum -= i;
+			} else {
+				System.out.print(" + " + i);
+				sum += i;
 			}
 		}
-		System.out.println(" = "+sum);
+		
+		System.out.println(" = " + sum);
 
 	}
 
---
+++ practice/src/practice/Main.java
@@ -0,0 +1,16 @@
+package practice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String str = br.readLine();
+		int num = Integer.parseInt(br.readLine());
+
+	}
+}
